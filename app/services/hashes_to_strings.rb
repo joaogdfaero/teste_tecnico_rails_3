0aecc9d18204aff7d5fe78e3eddacf92d8056fbe
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-04-06 07:41:54 -0300

    feat: hashs to strings

---
+++ app/services/hashes_to_strings.rb
@@ -0,0 +1,28 @@
+class TesteTecnico
+  def hashs_to_string(arg)
+    string = ''
+    n = 11
+
+    (0..arg.length - 1).each { |i|
+
+      (0..arg[0].length-1).each { |j|
+        if arg[i].to_a[j][1].length < n
+          arg[i][:name] = arg[i][:name].ljust(n," ")
+          arg[i][:cpf] = arg[i][:cpf].ljust(n," ")
+          arg[i][:state] = arg[i][:state].ljust(n," ")
+          arg[i][:value] = arg[i][:value].ljust(n," ")
+        end}
+
+      string << arg[i][:name][0..n-1] + arg[i][:cpf][0..n-1] + arg[i][:state][0..n-1] + ' ' + arg[i][:value][0..n-1]
+
+      if i != (arg.length - 1)
+        string << ' \n'
+      end
+    }
+
+    return string
+  end
+end
+
+
+
