c3bd65aad4c8f44faf5768c6669f6e9a5ef6ef51
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-04-15 09:42:04 -0300

    feat: hashs to strings working

--- app/services/hashes_to_strings.rb
+++ app/services/hashes_to_strings.rb
@@ -13,35 +13,12 @@ class TesteTecnico
           arg[i][:value] = arg[i][:value].ljust(n," ")
         end}
 
-      string << arg[i][:name][0..n-1] + arg[i][:cpf][0..n-1] + arg[i][:state][0..n-1] + ' ' + arg[i][:value][0..n-1]
+      string << arg[i][:name][0..n-1] + arg[i][:cpf][0..n-1] + arg[i][:state][0..n-1] + arg[i][:value][0..n-1]
 
       if i != (arg.length - 1)
         string << "\n"
       end
     }
-# "Maria Neusa97905796671Sao Paulo   1234       \nRicardo Fon44010762900Rio Grande  567        "
     return string
   end
 end
-
-input =
-[
-  { name: 'Maria Neusa de Aparecida',
-    cpf: '97905796671',
-    state: 'Sao Paulo',
-    value: '1234' },
-  { name: 'Ricardo Fontes',
-    cpf: '44010762900',
-    state: 'Rio Grande do Sul',
-    value: '567' }
-]
-
-teste = TesteTecnico.new
-result = "Maria Neusa97905796671Sao Paulo   1234       \nRicardo Fon44010762900Rio Grande  567        "
-
-if result = teste.hashs_to_string(input)
-  p "foi powwww"
-end
-
-# Maria Neusa97905796671Sao Paulo   1234        \nRicardo Fon44010762900Rio Grande  567
-
