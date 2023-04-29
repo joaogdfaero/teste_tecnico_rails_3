be47a91d6d5981ca94cf4a7c73f1910a4f55271b
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-04-29 09:52:23 -0300

    feat: hashs to strings yaml

--- app/services/hashs_to_strings_yaml.rb
+++ app/services/hashs_to_strings_yaml.rb
@@ -1,66 +1,79 @@
 #!/usr/bin/env ruby
 require 'yaml'
-class TesteTecnico
-  def initialize(arg)
-    @hashes = arg
-    @yamlconfig = YAML.load_file('exemplo1.yaml')
+class TesteTecnico3
+  def initialize(data, config_file_path)
+    @data = data
+    @config = YAML.load_file(config_file_path)
   end
 
-  # REFATORA O CÓDIGO COM AS SUGESTÕES DO CHATGPT
-  def hashs_to_string
+  def hashs_to_string_yaml
      string = ''
+     n_arr, n_cpf, n_name, n_state, n_value = get_name_length
 
-     n_name = @yamlconfig.fetch('name', {})['length'] || -100
-     n_cpf = @yamlconfig.fetch('cpf', {})['length'] || -100
-     n_state = @yamlconfig.fetch('state', {})['length'] || -100
-     n_value = @yamlconfig.fetch('value', {})['length'] || -100
-     n_arr = [n_name, n_cpf, n_state, n_value]
+     apply_config(n_arr, n_cpf, n_name, n_state, n_value, string)
 
-     (0..@hashes.length - 1).each { |i|
+     return string
+  end
 
-       (0..@hashes[0].length-1).each { |j|
-         if @hashes[i].to_a[j][1].length < n_arr[j]
-           if @yamlconfig.fetch('name', {})['align'] = 'left'
-             @hashes[i][:name] = @hashes[i][:name].ljust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:name] = @hashes[i][:name].ljust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           else
-             @hashes[i][:name] = @hashes[i][:name].rjust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:name] = @hashes[i][:name].rjust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           end
+  private
 
-           if @yamlconfig.fetch('cpf', {})['align'] = "left"
-             @hashes[i][:cpf] = @hashes[i][:cpf].ljust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:cpf] = @hashes[i][:cpf].ljust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           else
-             @hashes[i][:cpf] = @hashes[i][:cpf].rjust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:cpf] = @hashes[i][:cpf].rjust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           end
+  def apply_config(n_arr, n_cpf, n_name, n_state, n_value, string)
+    (0..@data.length - 1).each { |i|
+      (0..@data[0].length - 1).each { |j|
+        if @data[i].to_a[j][1].length < n_arr[j]
+          if @config['name']['align'] = 'left'
+            @data[i][:name] = @data[i][:name].ljust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:name] = @data[i][:name].ljust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          else
+            @data[i][:name] = @data[i][:name].rjust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:name] = @data[i][:name].rjust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          end
 
-           if @yamlconfig.fetch('state', {})['align'] = "left"
-             @hashes[i][:state] = @hashes[i][:state].ljust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:state] = @hashes[i][:state].ljust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           else
-             @hashes[i][:state] = @hashes[i][:state].rjust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:state] = @hashes[i][:state].rjust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           end
+          if @config['cpf']['align'] = "left"
+            @data[i][:cpf] = @data[i][:cpf].ljust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:cpf] = @data[i][:cpf].ljust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          else
+            @data[i][:cpf] = @data[i][:cpf].rjust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:cpf] = @data[i][:cpf].rjust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          end
 
-           if @yamlconfig.fetch('value', {})['align'] = "left"
-             @hashes[i][:value] = @hashes[i][:value].ljust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:value] = @hashes[i][:value].ljust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           else
-             @hashes[i][:value] = @hashes[i][:value].rjust(n_arr[j]," ") if (@yamlconfig.fetch('name', {})['padding'] == 'spaces')
-             @hashes[i][:value] = @hashes[i][:value].rjust(n_arr[j],"0") if (@yamlconfig.fetch('name', {})['padding'] == 'zeroes')
-           end
-         end}
+          if @config['state']['align'] = 'left'
+            @data[i][:state] = @data[i][:state].ljust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:state] = @data[i][:state].ljust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          else
+            @data[i][:state] = @data[i][:state].rjust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:state] = @data[i][:state].rjust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          end
 
-       string << @hashes[i][:name][0..n_name-1] + @hashes[i][:cpf][0..n_cpf-1] + @hashes[i][:state][0..n_state-1] + @hashes[i][:value][0..n_value-1]
+          if @config['value']['align'] = 'left'
+            @data[i][:value] = @data[i][:value].ljust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:value] = @data[i][:value].ljust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          else
+            @data[i][:value] = @data[i][:value].rjust(n_arr[j], " ") if (@config.fetch('name', {})['padding'] == 'spaces')
+            @data[i][:value] = @data[i][:value].rjust(n_arr[j], "0") if (@config.fetch('name', {})['padding'] == 'zeroes')
+          end
+        end }
 
-       if i != (@hashes.length - 1)
-         string << "\n"
-       end
-       }
+      write_strings(i, n_cpf, n_name, n_state, n_value, string)
+    }
+  end
 
-     p string
+  def write_strings(i, n_cpf, n_name, n_state, n_value, string)
+    string << @data[i][:name][0..n_name - 1] + @data[i][:cpf][0..n_cpf - 1] + @data[i][:state][0..n_state - 1] + @data[i][:value][0..n_value - 1]
+
+    if i != (@data.length - 1)
+      string << "\n"
+    end
+  end
+
+  def get_name_length
+    n_name = @config.fetch('name', {})['length'] || -100
+    n_cpf = @config.fetch('cpf', {})['length'] || -100
+    n_state = @config.fetch('state', {})['length'] || -100
+    n_value = @config.fetch('value', {})['length'] || -100
+    n_arr = [n_name, n_cpf, n_state, n_value]
+
+    return n_arr, n_cpf, n_name, n_state, n_value
   end
 end
 
@@ -76,5 +89,7 @@ input =
       value: '567' }
   ]
 
-teste = TesteTecnico.new(input)
-teste.hashs_to_string
+# config_file_path = 'exemplo1.yaml'
+#
+# teste = TesteTecnico.new(input, config_file_path)
+# p teste.hashs_to_string_yaml
